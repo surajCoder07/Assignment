ebb3ee7160545f94ef561ec28302314bbba34da2
surajCoder07 <116960530+surajCoder07@users.noreply.github.com>
2022-11-10 21:48:51 +0530

    Add files via upload

---
+++ Question.java
@@ -0,0 +1,23 @@
+package Assignment;
+
+public class Question {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int add;
+        int sub;
+        double multiply;
+        double divide;
+        a=8;
+        b=4;
+
+        add=a+b;
+        sub=a-b;
+        multiply=a*b;
+        divide=a/b;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(multiply);
+        System.out.println(divide);
+    }
+}
