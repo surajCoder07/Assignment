24fb8942d046ccb59c4407c02f9444bee1342860
surajCoder07 <116960530+surajCoder07@users.noreply.github.com>
2022-11-12 12:37:46 +0530

    Add files via upload

---
+++ Question3.java
@@ -0,0 +1,14 @@
+package Assignment;
+
+
+
+public class Question3 {
+    public static void main(String[] args) {
+
+        for(int i=10;i<=150;i++){
+            if(i%23==0){
+                System.out.println(i+" ");
+            }
+        }
+    }
+}
